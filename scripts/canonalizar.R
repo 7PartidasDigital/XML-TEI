9827e35852304d3c6aaa8c56255b12139d255f0c
7 Partidas Digital <7PartidasDigital@gmail.com>
2019-05-10 08:08:27 +0200

    Correcciones

--- scripts/canonalizar.R
+++ scripts/canonalizar.R
@@ -12,6 +12,7 @@
 
 
 library(tidyverse)
+files <- list.files("REGULARIZADO")
 LOP_entrada <- tolower(readLines("lopez.txt"))
 IOC_entrada <- tolower(readLines("montalvo.txt"))
 
@@ -19,11 +20,14 @@ IOC_entrada <- tolower(readLines("montalvo.txt"))
 
 charta <- function(x){
 corregido <- str_replace_all(prueba, "nrr", "nr")
+corregido <- str_replace_all(corregido, "\\bconu[ji]", "convi")
+corregido <- str_replace_all(corregido, "([bcdfghlmnpqrstv])y([bcdfghlmnpqrstv])", "\\1i\\2")
 corregido <- str_replace_all(corregido, "mm", "m")
 corregido <- str_replace_all(corregido, "n([pb])", "m\\1")
 corregido <- str_replace_all(corregido, "nn", "ñ")
 corregido <- str_replace_all(corregido, "ñj", "ñi")
 corregido <- str_replace_all(corregido, "ñ[^aeiou]", "n")
+corregido <- str_replace_all(corregido, "ñobl", "nobl")
 corregido <- str_replace_all(corregido, "ss", "s")
 corregido <- str_replace_all(corregido, "ff", "f")
 corregido <- str_replace_all(corregido, "\\&", "e")
@@ -34,6 +38,11 @@ corregido <- str_replace_all(corregido, "ç([ei])", "c\\1")
 corregido <- str_replace_all(corregido, "seer", "ser")
 corregido <- str_replace_all(corregido, "vn", "un")
 corregido <- str_replace_all(corregido, "ff", "f")
+corregido <- str_replace_all(corregido, " i ", " e ")
+corregido <- str_replace_all(corregido, " vs", " us")
+corregido <- str_replace_all(corregido, "\\bsalu([aeio])", "salv\\1")
+corregido <- str_replace_all(corregido, "cuy([dt])", "cui\\1")
+corregido <- str_replace_all(corregido, "ay([bcdfghjklmnpqrst])", "ai\\1")
 corregido <- str_replace_all(corregido, "\\bn([ijo])n\\b", "n\\1")
 corregido <- str_replace_all(corregido, "([aeiou])u([aeiou])", "\\1v\\2")
 corregido <- str_replace_all(corregido, "([aeiou])lu([aeiou])", "\\1v\\2")
@@ -41,7 +50,7 @@ corregido <- str_replace_all(corregido, "([^d][eo])s([cç][ei])", "\\1\\2")
 corregido <- str_replace_all(corregido, "([^lu]e)y([^aieou\b])", "\\1i\\2") # Falla con grey, y rey
 corregido <- str_replace_all(corregido, "rei\\b", "rey")
 corregido <- str_replace_all(corregido, "([aeou])i([aeou])", "\\1j\\2") # No lo tengo claro aún
-corregido <- str_replace_all(corregido, "quj", "qui")
+corregido <- str_replace_all(corregido, "([qg])uj", "\\1ui")
 corregido <- str_replace_all(corregido, "\\by([^aeiou])", "i\\1")
 corregido <- str_replace_all(corregido, "oy([dtrgms])", "oi\\1")
 corregido <- str_replace_all(corregido, "sy", "si")
@@ -51,16 +60,21 @@ corregido <- str_replace_all(corregido, "ent\\b", "ente")
 corregido <- str_replace_all(corregido, "honr", "onr")
 corregido <- str_replace_all(corregido, "\\bnome", "nombre")
 corregido <- str_replace_all(corregido, "\\bome", "ombre")
+corregido <- str_replace_all(corregido, "\\bhome\\b", "ombre")
+corregido <- str_replace_all(corregido, "\\bhomes\\b", "ombres")
+corregido <- str_replace_all(corregido, "([ie])ru[ji]", "\\1rvi")
+corregido <- str_replace_all(corregido, "\\bujese", "uviese")
+corregido <- str_replace_all(corregido, "\\bgran[dt]\\b", "gran")
 corregido <- str_replace_all(corregido, "\\bivez", "juez")
 corregido <- str_replace_all(corregido, "\\biuyz", "juiz")
 corregido <- str_replace_all(corregido, "\\biu[dz]", "juz")
 corregido <- str_replace_all(corregido, "gun[dt]\\b", "gun")
 corregido <- str_replace_all(corregido, "\\blei\\b", "ley")
 corregido <- str_replace_all(corregido, "\\bpley", "plei")
-#corregido <- str_replace_all(corregido, "\\bh", "")
 corregido <- str_replace_all(corregido, "\\biamas", "jamas")
-#corregido <- str_extract_all(corregido, "\\bu([aeiou])", "v\\1")
 corregido <- str_replace_all(corregido, "\\biu", "ju")
+corregido <- str_replace_all(corregido, "\\bdesuso\\b", "de suso")
+corregido <- str_replace_all(corregido, "\\bdeyuso\\b", "de yuso")
 }
 
 prueba <- IOC_entrada
@@ -70,35 +84,9 @@ limpio <- charta(prueba)
 writeLines(limpio, "montalvo_reg.txt")
 #EN IOC
 #limpio <- str_replace_all(limpio, "\\bf([ai])z", "\\1z")
+limpio <- str_replace_all(limpio, "ñoble", "noble")
 limpio <- str_replace_all(limpio, "\\bgaña", "gana")
 limpio <- str_replace_all(limpio, "\\bu([aeiou])", "v\\1")
 limpio <- str_replace_all(limpio, "\\bveste", "ueste")
 limpio <- str_replace_all(limpio, "\\bverta", "uerta")
 
-
-
-IOC <- tibble(texto = limpio)
-
-prueba <- LOP_entrada
-limpio <- charta(prueba)
-
-LOP <- tibble(texto = limpio)
-
-IOC <- IOC %>%
-  filter(!str_detect(texto, "^\\d+.*"))
-LOP <- LOP %>%
-  filter(!str_detect(texto, "^\\d+.*"))
-LOP$texto <- gsub(" folio \\.\\d+\\.$", "", LOP$texto) # BORRA FOL. NUM DE LOPEZ
-
-
-
-#rm(prueba)
-
-IOC_palabras <- IOC %>%
-  tidytext::unnest_tokens(palabra, texto) %>%
-  count(palabra, sort = T)
-LOP_palabras <- LOP %>%
-  tidytext::unnest_tokens(palabra, texto) %>%
-  count(palabra, sort = T)
-
-resto <- anti_join(IOC_palabras, LOP_palabras, by="palabra")
