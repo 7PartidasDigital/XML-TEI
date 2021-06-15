4f7083565014c8d169fd674b6814231576b6376c
7PartidasDigital <7PartidasDigital@gmail.com>
2021-06-15 13:17:48 +0200

    Correcciones en 1.0 1.1 1.2 y 1.3

--- scripts/limpia_colacion.R
+++ scripts/limpia_colacion.R
@@ -1,8 +1,26 @@
+# Convierte en tablas tsv los ficheros de salida
+# de Collatex eliminando las plecas y guiones que
+# crean la tabla de salida para leerlo en Excel
+# para que marque las diferencias.
+
 library(tidyverse)
-ficheros <- list.files()
-uno <- readLines("colacion_0100.txt")
-uno <- gsub("^\\| *", "", uno)
-uno <- gsub(" +\\|$", "", uno)
-uno <- gsub(" +\\| +", "\t", uno)
-uno <- gsub("^\\+-----.*$", "", uno)
-uno <- uno[uno !=""]
+ficheros <- list.files(pattern = ".txt")
+for(i in 1:length(ficheros)){
+  uno <- readLines(ficheros[i])
+  uno <- gsub("^\\| *", "", uno)
+  uno <- gsub(" +\\|$", "", uno)
+  uno <- gsub(" +\\| +", "\t", uno)
+  uno <- gsub("^\\+-----.*$", "", uno)
+  uno <- uno[uno !=""]
+  uno <- uno[-2:-9]
+  writeLines(uno, ficheros[i])
+}
+
+# El siguiente snippet lee las tabla y las convierte
+# en un Excel en el que cada titulo es una hoja.
+
+ficheros <- list.files(pattern = ".txt")
+hojas <- gsub("colacion_(.*)\\.txt", "\\1", ficheros)
+datos <- lapply(ficheros, read_tsv)
+names(datos) <- hojas
+writexl::write_xlsx(datos, "PRUEBA2.xlsx")
